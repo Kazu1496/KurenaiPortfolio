6d26a19e3fad06dfd02ec387c7dab5a1354174e4
Kazumasa Ishikura <kazumasa1496@gmail.com>
2018-05-20 20:31:32 +0900

    ローディング変更

--- javascript/loading.js
+++ javascript/loading.js
@@ -1,7 +1,7 @@
 $("#fakeLoader").fakeLoader({
-  timeToHide:2000, //Time in milliseconds for fakeLoader disappear
+  timeToHide:3000, //Time in milliseconds for fakeLoader disappear
   zIndex:999, // Default zIndex
   spinner:"spinner2",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
   bgColor:"#000", //Hex, RGB or RGBA colors
-  imagePath:"icon/loading02b_r1_c1.gif" //If you want can you insert your custom image
+  // imagePath:"icon/loading02b_r1_c1.gif" //If you want can you insert your custom image
 });
